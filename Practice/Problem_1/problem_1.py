1e0540f47c0788d1e9c13405b7b5b3e3bf26d598
Vlad Bojan <vlad.bojan0@gmail.com>
2024-04-08 11:06:50 +0300

    change solution for problem 1

--- Practice/Problem_1/problem_1.py
+++ Practice/Problem_1/problem_1.py
@@ -1,67 +1,80 @@
-import random
-
-n = int(input("Enter an integer larger than 15: "))
-numbers = [_ for _ in range(n)]
-
-# First generation of chromosomes
-POPULATION_SIZE = 10
-population = [random.sample(numbers, len(numbers)) for _ in range(POPULATION_SIZE)]
+import numpy as np
 
 
-# This fitness function counts and returns how many neighbors satisfy the condition that x+y>7
 def fitness(chromosome):
-    condition_satisfied_count = 0
+    counter = 0
     for i in range(len(chromosome)):
-        if i < len(chromosome) - 1:
-            if chromosome[i] + chromosome[i + 1] > 7:
-                condition_satisfied_count += 1
-        elif chromosome[i] + chromosome[0] > 7:
-            condition_satisfied_count += 1
-    return condition_satisfied_count
+        if chromosome[i - 1] + chromosome[i] > 7:
+            counter += 1
+    return counter
 
 
-# Roulette wheel selection
-def selection(generation):
-    # Make a list with the fitness of each chromosome in the current generation
-    fitness_levels = [fitness(chromosome) for chromosome in generation]
+def select(current_generation):
+    fitness_levels = [fitness(chromosome) for chromosome in current_generation]
     total_fitness = sum(fitness_levels)
 
-    # The proportion of the total fitness for each chromosome
-    fitness_proportions = [(fitness_level / total_fitness) for fitness_level in fitness_levels]
+    probabilities_of_being_chosen = [fitness_level / total_fitness for fitness_level in fitness_levels]
 
-    # When the cumulative probability will overtake this number, the corresponding chromosome will be returned
-    random_number = random.random()
+    random_probability = np.random.random()
 
-    # Create an environment in which the chromosomes with the higher proportion will
-    # have a higher probability of being chosen
     cumulative_probability = 0
-    for index, fitness_proportion in enumerate(fitness_proportions):
-        cumulative_probability += fitness_proportion
-        if random_number < cumulative_probability:
-            return current_generation[index]
+    for i in range(len(current_generation)):
+        cumulative_probability += probabilities_of_being_chosen[i]
+        if random_probability <= cumulative_probability:
+            return current_generation[i]
     return current_generation[-1]
 
 
-def crossover(chromosome1, chromosome2):
-    crossover_point = random.randint(1, len(chromosome1) - 1)
-    offspring = chromosome1[:crossover_point] + chromosome2[crossover_point:]
-    return offspring
+def crossover(parent1, parent2):
+    crossover_point = np.random.randint(1, len(parent1))
+    return parent1[:crossover_point] + parent2[crossover_point:]
 
 
-current_generation = population
-for _ in range(10000):
-    improved_generation = []
-    for __ in range(POPULATION_SIZE):
-        # Create chromosome parents, so I can create a better chromosome offspring
-        parent1 = selection(current_generation)
-        parent2 = selection(current_generation)
-        child = crossover(parent1, parent2)
-        improved_generation.append(child)
-    current_generation = improved_generation
+def mutate(unmutated_child, input_list):
+    mutated_child = []
 
-# The chromosome with the highest fitness from the current generation
-fitness_levels = [fitness(chromosome) for chromosome in current_generation]
-best_chromosome_index = fitness_levels.index(max(fitness_levels))
-best_chromosome = current_generation[best_chromosome_index]
+    mutation_rate = 0.1
 
-print(f"{best_chromosome} -> {fitness(best_chromosome)}")
+    for gene in unmutated_child:
+        random_probability = np.random.random()
+        if random_probability <= mutation_rate:
+            mutated_child.append(np.random.choice(input_list))
+        else:
+            mutated_child.append(gene)
+
+    return mutated_child
+
+
+def main():
+    # Get input
+    n = int(input("Enter a value larger than 15: "))
+    input_list = []
+    for i in range(n):
+        input_element = int(input(f"input_list[{i}] = "))
+        input_list.append(input_element)
+
+    # Generate initial generation
+    current_generation = [list(np.random.permutation(input_list)) for _ in range(10)]
+
+    # Run genetic algorithm for 10 thousand iterations
+    for _ in range(10000):
+        improved_generation = []
+        for _ in range(n):
+            parent1 = select(current_generation)
+            parent2 = select(current_generation)
+            unmutated_child = crossover(parent1, parent2)
+            child = mutate(unmutated_child, input_list)
+            improved_generation.append(child)
+        current_generation = improved_generation
+
+    # Find the best chromosome in the last generation
+    fitness_levels = [fitness(chromosome) for chromosome in current_generation]
+    maximum_fitness = max(fitness_levels)
+    best_chromosome_index = fitness_levels.index(maximum_fitness)
+    best_chromosome = current_generation[best_chromosome_index]
+
+    # Display best chromosome
+    print(f"Best chromosome is {best_chromosome} with a fitness of {maximum_fitness}")
+
+
+main()
